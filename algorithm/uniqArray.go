e93efa5af4031cd1f69c8d3361c7bba2f4f67177
skykeeper0 <trungdinh0429@gmail.com>
2017-05-14 12:39:19 -0700

    add highOrderFunc

--- algorithm/uniqArray.go
+++ algorithm/uniqArray.go
@@ -2,10 +2,26 @@
 
 package main
 
-func uniqArray() {
+import "fmt"
 
+func uniqArray(array []int) []int {
+	var store = map[int]int{}
+	var resultArr = []int{}
+
+	for _, value := range array {
+		freq, exist := store[value]
+		if exist {
+			store[value] = freq + 1
+		} else {
+			store[value] = 1
+			resultArr = append(resultArr, value)
+		}
+	}
+
+	return resultArr
 }
 
 func main() {
-
+	var arr = []int{1, 3, 4, 7, 2, 2, 10, 7, 10}
+	fmt.Println(uniqArray(arr))
 }
