75d763e10e6abb48693f95a2a3b54ed428d17521
skykeeper0 <trungdinh0429@gmail.com>
2017-05-14 13:10:06 -0700

    add factorial into recursion

--- recursion/factorial.go
+++ recursion/factorial.go
@@ -1,9 +1,15 @@
 package main
 
-func factorial(x int) int {
+import "fmt"
 
+func factorial(x int) int {
+	if x == 1 {
+		return 1
+	}
+
+	return x * factorial(x-1)
 }
 
 func main() {
-
+	fmt.Println(factorial(5))
 }
